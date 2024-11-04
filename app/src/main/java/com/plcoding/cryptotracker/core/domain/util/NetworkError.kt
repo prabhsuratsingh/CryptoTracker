f3e0e53a838bd9baf34675c6918de814f8151fdf
prabhsuratsingh <prabhsurat17@gmail.com>
2024-11-05 00:21:36 +0530

    setup url and network helpers and modified build.gradle(:app)

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/domain/util/NetworkError.kt
@@ -0,0 +1,9 @@
+package com.plcoding.cryptotracker.core.domain.util
+
+enum class NetworkError : Error {
+    REQUEST_TIMEOUT,
+    TOO_MANY_REQUEST,
+    SERVER_ERROR,
+    SERIALIZATION,
+    UNKNOWN
+}
