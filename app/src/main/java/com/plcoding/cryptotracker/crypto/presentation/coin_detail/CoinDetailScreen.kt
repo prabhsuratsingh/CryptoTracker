9a812a2bd64b97514daebea49fadd4994f01eb9c
prabhsuratsingh <prabhsurat17@gmail.com>
2024-11-12 00:37:06 +0530

    created Coin InfoCard

--- app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/CoinDetailScreen.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/presentation/coin_detail/CoinDetailScreen.kt
@@ -1,20 +1,30 @@
 package com.plcoding.cryptotracker.crypto.presentation.coin_detail
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.CoinListState
 import com.plcoding.cryptotracker.crypto.presentation.coin_list.components.previewCoin
 import com.plcoding.cryptotracker.ui.theme.CryptoTrackerTheme
@@ -46,7 +56,30 @@ fun CoinDetailScreen(
                 .verticalScroll(rememberScrollState())
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {  }
+        ) {
+            Icon(
+                imageVector = ImageVector.vectorResource(coin.iconRes),
+                contentDescription = coin.name,
+                modifier = Modifier.size(100.dp),
+                tint = MaterialTheme.colorScheme.primary
+            )
+
+            Text(
+                text = coin.name,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Black,
+                textAlign = TextAlign.Center,
+                color = contentColor
+            )
+
+            Text(
+                text = coin.symbol,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Light,
+                textAlign = TextAlign.Center,
+                color = contentColor
+            )
+        }
     }
 }
 
@@ -56,7 +89,10 @@ private fun CoinDetailScreenPreview() {
     CryptoTrackerTheme {
         CoinDetailScreen(
             state = CoinListState(
-                selectedCoin = previewCoin
+                selectedCoin = previewCoin,
+            ),
+            modifier = Modifier.background(
+                MaterialTheme.colorScheme.background
             )
         )
     }
